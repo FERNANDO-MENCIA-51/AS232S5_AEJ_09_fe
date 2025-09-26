8fa5bacfd743d9ca4ccab46f31f94e32896c16d2
Luis Mencia <luis.mencia@vallegrande.edu.pe>
2025-09-26 14:39:14 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'AS232S5_AEJ_09-fe';
+}
